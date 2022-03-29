03acf822df601ed3c0ad3c29954d3dd45fedc4cc
finalLaugh <futureisahead@gmail.com>
2022-03-29 16:04:23 +0300

    Version 1.0.2 - Finished the Page Object Model framework.

---
+++ src/test/java/epiesa/tests/HomepageTests.java
@@ -0,0 +1,23 @@
+package epiesa.tests;
+
+import epiesa.pages.HomePage;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomepageTests extends BaseTest {
+    HomePage homePage = new HomePage();
+
+    @Test(description = "Verify page title")
+    public void homePageTitle() {
+        String pageTitle = homePage.getPageTitle();
+        Assert.assertEquals(pageTitle, "Catalog piese auto online | +50 magazine in tara", "The title did not match.");
+    }
+
+    @Test(description = "Verify welcome message")
+    public void homePageGreeting() {
+        String greeting = homePage.getWelcomeMessage();
+        Assert.assertEquals(greeting, "LA PRET FOARTE BUN!", "The greeting didn't match.");
+        Assert.assertTrue(greeting.contains("PRET"), "Welcome message did not match!");
+    }
+}
